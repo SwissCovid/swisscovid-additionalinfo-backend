160426e9255bfee3cb238277b3c72a0429f6183a
alig <alig@ubique.ch>
2020-09-14 17:35:39 +0200

    Less Maxage.

--- dpppt-additionalinfo-backend/src/main/java/org/dpppt/additionalinfo/backend/ws/cache/CacheConfig.java
+++ dpppt-additionalinfo-backend/src/main/java/org/dpppt/additionalinfo/backend/ws/cache/CacheConfig.java
@@ -3,12 +3,12 @@ package org.dpppt.additionalinfo.backend.ws.cache;
 /** Holds the request header cache constants */
 public class CacheConfig {
 
-    private static final int SECOND = 1;
-    private static final int MINUTE = 60 * SECOND;
-    private static final int HOUR = 60 * MINUTE;
-    private static final int DAY = 24 * HOUR;
-    private static final int YEAR = 365 * DAY;
+	private static final int SECOND = 1;
+	private static final int MINUTE = 60 * SECOND;
+	private static final int HOUR = 60 * MINUTE;
+	private static final int DAY = 24 * HOUR;
+	private static final int YEAR = 365 * DAY;
 
-    public static final int NEXT_REFRESH_STATISTICS = 1 * DAY;
-    public static final int MAX_AGE_STATISTICS = 7 * DAY;
+	public static final int NEXT_REFRESH_STATISTICS = 1 * DAY;
+	public static final int MAX_AGE_STATISTICS = 5 * MINUTE;
 }
