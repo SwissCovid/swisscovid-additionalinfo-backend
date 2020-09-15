a85761be9975eafef6b4f2780468a3aea985412b
alig <alig@ubique.ch>
2020-09-15 14:38:21 +0200

    Add lastUpdated Field to json.

--- dpppt-additionalinfo-backend/src/main/java/org/dpppt/additionalinfo/backend/ws/statistics/MockStatisticClient.java
+++ dpppt-additionalinfo-backend/src/main/java/org/dpppt/additionalinfo/backend/ws/statistics/MockStatisticClient.java
@@ -5,22 +5,23 @@ import org.dpppt.additionalinfo.backend.ws.model.statistics.Statistics;
 import org.joda.time.LocalDate;
 
 public class MockStatisticClient implements StatisticClient {
-    @Override
-    public Statistics getStatistics() {
-        Statistics statistics = new Statistics();
-        statistics.setTotalActiveUsers(1623942);
-        LocalDate today = LocalDate.now();
-        LocalDate dayDate = LocalDate.now().minusDays(21);
-        for (int i = 0; dayDate.isBefore(today); i++) {
-            History history = new History();
-            history.setDate(dayDate);
-            history.setCovidcodesEntered(50 + i * 2);
-            history.setNewInfections((int) (210 + Math.pow(i, 2)));
-            history.setNewInfectionsSevenDayAverage((int) (250 + Math.pow(i, 2)));
-            statistics.getHistory().add(history);
+	@Override
+	public Statistics getStatistics() {
+		LocalDate today = LocalDate.now();
+		Statistics statistics = new Statistics();
+		statistics.setTotalActiveUsers(1623942);
+		statistics.setLastUpdated(today);
+		LocalDate dayDate = LocalDate.now().minusDays(21);
+		for (int i = 0; dayDate.isBefore(today); i++) {
+			History history = new History();
+			history.setDate(dayDate);
+			history.setCovidcodesEntered(50 + i * 2);
+			history.setNewInfections((int) (210 + Math.pow(i, 2)));
+			history.setNewInfectionsSevenDayAverage((int) (250 + Math.pow(i, 2)));
+			statistics.getHistory().add(history);
 
-            dayDate = dayDate.plusDays(1);
-        }
-        return statistics;
-    }
+			dayDate = dayDate.plusDays(1);
+		}
+		return statistics;
+	}
 }
